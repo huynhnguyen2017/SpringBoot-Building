6905247466c145c89c8cff3c42ae253a133bf63d
huynhnguyen2017 <huynh201720172017@gmail.com>
2022-05-28 11:46:44 +0700

    initialize projects

---
+++ src/main/java/com/example/SpringBootBuilding/SpringBootBuildingApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootBuilding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBuildingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBuildingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootBuilding/SpringBootBuildingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootBuilding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBuildingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
